4f62e70b89749a5aec81a5a4bb3c6506660c6626
olimarco <olimarco999@gmail.com>
2026-05-18 20:21:18 +0200

    Implementation of HeapNode and Array classes

--- DataStructures.py
+++ DataStructures.py
@@ -40,31 +40,56 @@ class LinkedList:
         return self.size    
     
 
-    class Queue:
-        def __init__(self):
-            self.head = None
-            self.tail = None
-            self.size = 0
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
         
-        def enqueue(self, item):
-            new_node = Node(item)
-            if not self.head:
-                self.head = new_node
-                self.tail = new_node
-            else:
-                self.tail.next = new_node
-                self.tail = new_node
-            self.size += 1
+    def enqueue(self, item):
+        new_node = Node(item)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
 
-        def dequeue(self):
-            if not self.head:
-                return None
-            dequeued_item = self.head.data
-            self.head = self.head.next
-            if not self.head:
-                self.tail = None
-            self.size -= 1
-            return dequeued_item
+    def dequeue(self):
+        if not self.head:
+            return None
+        dequeued_item = self.head.data
+        self.head = self.head.next
+        if not self.head:
+            self.tail = None
+        self.size -= 1
+        return dequeued_item
         
-        def is_empty(self):
-            return self.size == 0
+    def is_empty(self):
+        return self.size == 0
+    
+
+class HeapNode(Node):
+    def __init__(self, data, priority):
+        super().__init__(data)
+        self.priority = priority
+
+
+class Array:
+    def __init__(self, size):
+        self.size = size
+        self.array = [None] * size
+
+    def get(self, index):
+        if index < 0 or index >= self.size:
+            raise IndexError("Indice fuori dai limiti della memoria")
+        return self.array[index]
+    
+    def set(self, index, value):
+        if index < 0 or index >= self.size:
+            raise IndexError("Indice fuori dai limiti della memoria")
+        self.array[index] = value
+
+    def length(self):
+        return self.size
