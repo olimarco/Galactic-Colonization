8f6093c478bac4858e23df074f3f5437346472df
olimarco <olimarco999@gmail.com>
2026-05-25 15:44:51 +0200

    spaceship

--- GameModels.py
+++ GameModels.py
@@ -16,6 +16,7 @@ class Sector:
     def get_danger_probability(self):
         return self.danger_level
 
+
 class Route:
     def __init__(self, destination, fuel_cost):
         self.destination = destination
@@ -26,3 +27,23 @@ class Route:
 
     def get_destination(self):
         return self.destination
+
+
+class Spaceship:
+    def __init__(self, current_sector, fuel):
+        self.current_sector = current_sector
+        self.fuel = fuel
+        self.collected_resources = 0
+
+    def move_to(self, s, cost):
+        self.deduct_fuel(cost)
+        self.current_sector = s
+    
+    def deduct_fuel(self, amount):
+        self.fuel -= amount
+    
+    def add_resources(self, amount):
+        self.collected_resources += amount
+
+    def is_operational(self):
+        return self.fuel
